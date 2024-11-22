ed110ab762beac7f7de1d1dc32d9b6371bd87825
Samuel <sksksjakskska@gmail.com>
2024-11-23 01:05:19 +0900

    Feat: Hello World

---
+++ src/main/java/com/homepage/lifescuplture/LifeScupltureApplication.java
@@ -0,0 +1,13 @@
+package com.homepage.lifescuplture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeScupltureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeScupltureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homepage/lifescuplture/LifeScupltureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homepage.lifescuplture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeScupltureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
